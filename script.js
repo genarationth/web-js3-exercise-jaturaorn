c64356f6d8e585ee9471d8a3aacb498d7c174118
jaturaporn <83279262+jaturaorn@users.noreply.github.com>
2023-03-14 07:45:29 +0000

    add Exercise #2 Part 2

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
-const calcost = (money,transation, interest) => {
-    return money > 0 || transation >= 0 || interest >= 0 ? (money*transation)*interest:
-    console.log('Please enter positive number');
+// const calcost = (money,transation, interest) => {
+//     return money > 0 || transation >= 0 || interest >= 0 ? (money*transation)*interest:
+//     console.log('Please enter positive number');
+// }
+
+// console.log(calcost(10000,3.0,0.01));
+
+// const welguest = (name1,name2,name3) => {
+//     console.log(`Welcome ${name1},${name2},${name3}`);
+// }
+
+// welguest('Joe','Jack','Rohan')
+
+const calage = (currentYear,birthYear) => {
+    return currentYear - birthYear;
 }
-
-console.log(calcost(10000,3.0,0.01));
-
-const welguest = (name1,name2,name3) => {
-    console.log(`Welcome ${name1},${name2},${name3}`);
-}
-
-welguest('Joe','Jack','Rohan')
+console.log(calage(2023,1998));
